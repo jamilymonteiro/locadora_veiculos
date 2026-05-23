05042b45d1c9c2980ceaf2ef3fd64dd6ddc177a5
nayara <vitorianayy6@gmail.com>
2026-05-22 20:03:28 -0400

    feat/ icones

--- includes/menu.php
+++ includes/menu.php
@@ -5,23 +5,23 @@
     </h2>
 
     <a href="/locadora_veiculos/index.php">
-        Home
+        <i class="fas fa-house"></i> Home
     </a>
 
     <a href="/locadora_veiculos/veiculoUser.php">
-        Veículos
+        <i class="fas fa-car"></i> Veículos
     </a>
 
     <a href="/locadora_veiculos/aluguelUser.php">
-        Aluguéis
+        <i class="fas fa-key"></i> Aluguéis
     </a>
 
     <a href="/locadora_veiculos/contratoUser.php">
-        Contratos
+        <i class="fas fa-file-signature"></i> Contratos
     </a>
 
     <a href="/locadora_veiculos/login/login.php">
-        Admin
+        <i class="fas fa-user-shield"></i> Admin
     </a>
 
 </div>
--- includes/menuAdmin.php
+++ includes/menuAdmin.php
@@ -3,35 +3,35 @@
     <h2 class="logo_titulo">Royal Car</h2>
 
     <a href="/locadora_veiculos/admin.php">
-        Home
+        <i class="fas fa-house"></i> Home
     </a>
     
     <a href="/locadora_veiculos/cliente/listarCliente.php">
-        Clientes
+        <i class="fas fa-users"></i> Clientes
     </a>
 
     <a href="/locadora_veiculos/marca/listarMarca.php">
-        Marcas
+        <i class="fas fa-tag"></i> Marcas
     </a>
 
     <a href="/locadora_veiculos/categoria/listarCategoria.php">
-        Categorias
+        <i class="fas fa-list"></i> Categorias
     </a>
 
     <a href="/locadora_veiculos/veiculo/listarVeiculo.php">
-        Veículos
+        <i class="fas fa-car"></i> Veículos
     </a>
 
     <a href="/locadora_veiculos/aluguel/listarAluguel.php">
-        Aluguéis
+        <i class="fas fa-key"></i> Aluguéis
     </a>
 
     <a href="/locadora_veiculos/contrato/listarContrato.php">
-        Contratos
+        <i class="fas fa-file-signature"></i> Contratos
     </a>
 
     <a href="/locadora_veiculos/index.php">
-        Sair
+        <i class="fas fa-sign-out-alt"></i> Sair
     </a>
 
 </div>
